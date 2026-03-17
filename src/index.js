460d72f1473c869eaeef147e4eb85589fe72d9ed
quietwas <r3dmike@gmail.com>
2026-03-16 20:07:14 -0400

    Styled initial load of website
    Removed delay effect of loading styles using webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert("WE BACK")
+import "./styles.css"
+
+// body display is set to none originally and then set back to block once the js is added so there is no visual stutter
+const body = document.querySelector("body")
+body.style.display = "block"
