6012750c3119a9e85849e0a4d134263a852d95bb
quietwas <r3dmike@gmail.com>
2026-04-02 10:36:01 -0400

    Added some test styling and new JS files for each module
    Going to work on the home feature first and loading the home page initially

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import "./styles.css"
-
-// body display is set to none originally and then set back to block once the js is added so there is no visual stutter
-const body = document.querySelector("body")
-body.style.display = "block"
+import "./load.js"
