e6037b3677cec8c8175a4a7c96cbcd4b2018bb50
quietwas <r3dmike@gmail.com>
2026-04-03 19:56:54 -0400

    Initialized home.js and initial page load
    Created a button function which allows the user to change to the home screen once the other buttons are initialized and exported the createHomeScreen function to have an initial start screen

--- src/home.js
+++ src/home.js
@@ -1,1 +1,40 @@
 import "./styles.css"
+
+function createHomeScreen(content) {
+    content.replaceChildren()
+
+    const titleCard = document.createElement("div")
+    titleCard.classList.add("card")
+    const title = document.createElement("h2")
+    title.textContent = "Jezebel's"
+    titleCard.appendChild(title)
+
+    const reviewCard = document.createElement("div")
+    reviewCard.classList.add("card")
+    const review = document.createElement("p")
+    const author = document.createElement("p")
+    review.textContent = "This place ain't too half bad."
+    author.textContent = "- Infernus"
+    reviewCard.appendChild(review)
+    reviewCard.appendChild(author)
+
+    const hoursCard = document.createElement("div")
+    hoursCard.classList.add("card")
+    const hours = document.createElement("p")
+    hours.textContent = "Everyday 12pm-4am"
+    hoursCard.appendChild(hours)
+
+    content.appendChild(titleCard)
+    content.appendChild(reviewCard)
+    content.appendChild(hoursCard)
+}
+
+const homeButton = document.querySelector(".home")
+
+homeButton.addEventListener("click", () => {
+    createHomeScreen(content)
+})
+
+export {
+    createHomeScreen
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./load.js"
+import "./home.js"
+import { createHomeScreen } from "./home.js"
+
+const content = document.querySelector("#content")
+
+createHomeScreen(content)
